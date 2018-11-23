40c702eca5ba3995b4a43aa35684501aee51d27d
JJ Ryan <jjryan111@yahoo.com>
2018-11-23 14:17:29 -0500

    Initial Commit

---
+++ eBayInventory/Controllers/itemsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eBayInventory;
+
+namespace eBayInventory.Controllers
+{
+    public class itemsController : Controller
+    {
+        private ebaydbEntities db = new ebaydbEntities();
+
+        // GET: items
+        public ActionResult Index()
+        {
+            var items = db.items.Include(i => i.itemtype1);
+            return View(items.ToList());
+        }
+
+        // GET: items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item item = db.items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: items/Create
+        public ActionResult Create()
+        {
+            ViewBag.ItemType = new SelectList(db.itemtypes, "ItemTypeID", "TypeDesc");
+            return View();
+        }
+
+        // POST: items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,ItemType,ItemDesc,Paid,EstValue,Length,Width,Height,Weight")] item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ItemType = new SelectList(db.itemtypes, "ItemTypeID", "TypeDesc", item.ItemType);
+            return View(item);
+        }
+
+        // GET: items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item item = db.items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ItemType = new SelectList(db.itemtypes, "ItemTypeID", "TypeDesc", item.ItemType);
+            return View(item);
+        }
+
+        // POST: items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,ItemType,ItemDesc,Paid,EstValue,Length,Width,Height,Weight")] item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ItemType = new SelectList(db.itemtypes, "ItemTypeID", "TypeDesc", item.ItemType);
+            return View(item);
+        }
+
+        // GET: items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item item = db.items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            item item = db.items.Find(id);
+            db.items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eBayInventory/Views/itemtypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eBayInventory;
+
+namespace eBayInventory.Views
+{
+    public class itemtypesController : Controller
+    {
+        private ebaydbEntities db = new ebaydbEntities();
+
+        // GET: itemtypes
+        public ActionResult Index()
+        {
+            return View(db.itemtypes.ToList());
+        }
+
+        // GET: itemtypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            itemtype itemtype = db.itemtypes.Find(id);
+            if (itemtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemtype);
+        }
+
+        // GET: itemtypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: itemtypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemTypeID,TypeDesc")] itemtype itemtype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.itemtypes.Add(itemtype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemtype);
+        }
+
+        // GET: itemtypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            itemtype itemtype = db.itemtypes.Find(id);
+            if (itemtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemtype);
+        }
+
+        // POST: itemtypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemTypeID,TypeDesc")] itemtype itemtype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemtype).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemtype);
+        }
+
+        // GET: itemtypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            itemtype itemtype = db.itemtypes.Find(id);
+            if (itemtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemtype);
+        }
+
+        // POST: itemtypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            itemtype itemtype = db.itemtypes.Find(id);
+            db.itemtypes.Remove(itemtype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
